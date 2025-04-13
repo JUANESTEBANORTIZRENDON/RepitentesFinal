b773db07babff3b4c45cfd9a8e29cdfc9b56fa49
JEYomboy <JUANESTEBANORTIZRENDON24072004@gmail.com>
2025-04-12 21:11:45 -0500

    Actualizacion de Navbar

--- TiendaVirtual/TiendaVirtual/Controllers/HomeController.cs
+++ TiendaVirtual/TiendaVirtual/Controllers/HomeController.cs
@@ -22,6 +22,10 @@ namespace TiendaVirtual.Controllers
         {
             return View();
         }
+        public IActionResult Ventas()
+        {
+            return View();
+        }
 
         [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
         public IActionResult Error()
--- TiendaVirtual/TiendaVirtual/obj/Debug/net8.0/TiendaVirtual.AssemblyInfo.cs
+++ TiendaVirtual/TiendaVirtual/obj/Debug/net8.0/TiendaVirtual.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("TiendaVirtual")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+b3b00b55259cae03837fb426f7437ab2fcaee997")]
 [assembly: System.Reflection.AssemblyProductAttribute("TiendaVirtual")]
 [assembly: System.Reflection.AssemblyTitleAttribute("TiendaVirtual")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]

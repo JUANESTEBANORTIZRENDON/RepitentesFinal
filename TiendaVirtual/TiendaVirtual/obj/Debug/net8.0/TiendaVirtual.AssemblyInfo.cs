3f316a946f6d462aad83092d4dee276c6ac577c4
JEYomboy <JUANESTEBANORTIZRENDON24072004@gmail.com>
2025-04-27 21:44:40 -0500

    se acaba de configurar login para uso en red local y configuracion de direccionamiento de vista segun el rol

--- TiendaVirtual/TiendaVirtual/obj/Debug/net8.0/TiendaVirtual.AssemblyInfo.cs
+++ TiendaVirtual/TiendaVirtual/obj/Debug/net8.0/TiendaVirtual.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("TiendaVirtual")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+e43a7a9b1159f8bc0a4307ccdd9769cccf2aeabe")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+c47d339f6700497706e343b8e1754b7d7004aa27")]
 [assembly: System.Reflection.AssemblyProductAttribute("TiendaVirtual")]
 [assembly: System.Reflection.AssemblyTitleAttribute("TiendaVirtual")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]

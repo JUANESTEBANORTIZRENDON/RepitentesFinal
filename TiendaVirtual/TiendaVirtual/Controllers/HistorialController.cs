9f239754fa1f62eb670f4ca88c49fa17af0decdf
JEYomboy <JUANESTEBANORTIZRENDON24072004@gmail.com>
2025-05-16 11:19:01 -0500

    Se sube funsionalidades de carrito,facturacion de pdf y historial

---
+++ TiendaVirtual/TiendaVirtual/Controllers/HistorialController.cs
@@ -0,0 +1,44 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using TiendaVirtual.Data;
+using TiendaVirtual.Models;
+
+namespace TiendaVirtual.Controllers
+{
+    public class HistorialController : Controller
+    {
+        private readonly ApplicationDbContext _context;
+
+        public HistorialController(ApplicationDbContext context)
+        {
+            _context = context;
+        }
+
+        public async Task<IActionResult> Index(int pagina = 1, int tamanoPagina = 5)
+        {
+            int idUsuario = ObtenerIdUsuario();
+            var db = new DBHistorial(_context);
+
+            var historialCompleto = await db.ObtenerHistorialPorUsuarioAsync(idUsuario);
+
+            int totalRegistros = historialCompleto.Count;
+            var historialPaginado = historialCompleto
+                .Skip((pagina - 1) * tamanoPagina)
+                .Take(tamanoPagina)
+                .ToList();
+
+            ViewBag.TotalPaginas = (int)Math.Ceiling((double)totalRegistros / tamanoPagina);
+            ViewBag.PaginaActual = pagina;
+
+            return View(historialPaginado);
+        }
+
+
+        private int ObtenerIdUsuario()
+        {
+            var claim = User.FindFirst(System.Security.Claims.ClaimTypes.NameIdentifier);
+            return int.Parse(claim?.Value ?? "0");
+        }
+
+    }
+}
+
---
+++ TiendaVirtual/TiendaVirtual/Data/DBHistorial.cs
@@ -0,0 +1,24 @@
+﻿using TiendaVirtual.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace TiendaVirtual.Data
+{
+    public class DBHistorial
+    {
+        private readonly ApplicationDbContext _context;
+
+        public DBHistorial(ApplicationDbContext context)
+        {
+            _context = context;
+        }
+
+        public async Task<List<HistorialCompra>> ObtenerHistorialPorUsuarioAsync(int idUsuario)
+        {
+            return await _context.HistorialCompras
+                .Include(h => h.IdFacturaNavigation)
+                .Where(h => h.IdUsuario == idUsuario)
+                .OrderByDescending(h => h.Fecha)
+                .ToListAsync();
+        }
+    }
+}
